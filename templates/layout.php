33c0f523f3d6464d4055fa91675d95db8db2d763
Kir Koov <k.koovshinov@gmail.com>
2018-10-25 19:08:51 +0300

    Terms of use reworked

--- templates/layout.php
+++ templates/layout.php
@@ -34,11 +34,11 @@
         </div>
         <nav>
           <ul>
+            <li><a href=".sec01">about</a></li>
             <li><a href=".sec02">themed torfs</a></li>
             <li><a href=".sec03">add a torf</a></li>
-            <li><a href=".sec04">about</a></li>
-            <li><a href=".sec05">terms</a></li>
-            <li><a href=".sec06">@</a></li>
+            <li><a href=".sec04">terms</a></li>
+            <li><a href=".sec05">@</a></li>
 
 
           </ul>
@@ -56,31 +56,33 @@
     </main>
 
     <div class="row content">
-      <!-- <h1 class="sec01">?</h1>
-      <p></p> -->
-      <h1 class="sec02">themed torfs</h1>
-      <p>These are arguably better, since they enjoy common topics to choose from. First, opt for one and you are good to go in the same fashion as the random quiz above. Topic buttons follow, click whichever feels interesting at the moment.</p>
-      <h1 class="sec03">add a torf</h1>
-      <p>You have found out something you did not know was true or false? You are open to share it? Go on and add your own 'I-didn't-know-that!' torfs! Once you are through with a nightmare of a filtering web-form below, your torf will get inserted in our torf database under your email. The latter is used for further reference or removal of your torfs you no longer feel anxious about. Please pay special attention to your sources, NOT from Wikipedia please, and your torf users are sure to be very thankful if they would want to check something up later on. Start typing in the form fields and your data may show up in the dropdowns.</p>
-      <h1 class="sec04">about</h1>
-      <p>It all starts with a play. You are given a random true and false (<i>torf</i>) quiz of the day. All you have to do is read a torf statement in the centre and then choose whether it is true or false by pressing the corresponding T or F buttons on the sides. Its sources are somewhere easy to find, progress is arguably very necessary to display, and timing is of no importance (at least so far). As to the number of torfs, well it will be up to the AI behind the website)). Main thing is enjoy it or comment it, or try both.</p>
-      <p>This project was originally thought of as one embracing many a language, then it dropped down to only English, yet quiz enthusiasts are welcome to add more torfs translated from other languages. The main idea behind this project is encourage reading of more books and sharing knowledge thus gained, via torf quizzes. Comments are also very welcome, since they may really help in defining further course of action.</p>
+      <h1 class="sec01">about</h1>
+      <p>It all starts with a play. You are given a random true and false (<i>torf</i> for short) quiz of the day. All you have to do is read a statement in the centre and click it true or false by pressing the corresponding buttons on the sides. The answer comes referenced, progress is arguably very necessary to display, and timing is of no importance (at least so far). As to the number of torfs, well it will be up to the AI behind the website)).</p>
+      <p>Main thing is enjoy it or comment it, or try both.</p>
+      <p>This project was originally thought of as one embracing many a language, then it dropped down to English only since it is a universal language. Yet quiz enthusiasts are welcome to add more own torfs translated from other languages, provided they comply with the terms of use. The main idea behind this project is encourage users to read more books and share their newly gained knowledge for their won sake and others' too, via torf quizzes. Comments are also very welcome, since they may really help in defining further course of action.</p>
       <p>This small app demoes: MVC logic in a <a href="https://en.wikipedia.org/wiki/Matryoshka_doll">matryoshka doll</a> fashion to both separate inner workings vs views and avoid code repetition, interaction with a MySQL database via PDOs (PHP Data Objects)...</p>
-      <h1 class="sec05">terms</h1>
+      <h1 class="sec02">themed torfs</h1>
+      <p>These are arguably better, since they enjoy common topics to choose from. First, click on one and you are good to go in the same fashion as the random quiz above. Click whichever feels interesting at the moment.</p>
+      <h1 class="sec03">add a torf</h1>
+      <p>You have found out something you did not know and are open to share it? Go on and add your own 'I-didn't-know-that!' torfs! Once you are through with a nightmare of a filtering web-form below, your torf will populate the website's database under your email as its ID. This is good for further reference or removal of one's torfs one no longer feels anxious about sharing.</p>
+      <p>Special attention to be paid to ones' sources, that they do NOT refer to Wikipedia, to make happy other torf quiz users if they would want to check something up later on. A Wiki-hater script will regularly check the database for Wiki links. No offence, but real books in both physical and electronic nature are solid references, whereas Wikipedia, being great at giving general ideas and unveiling the unknown, so far seems unprofessional and unportected from very young contributors or "editors" who may add whatever they like.</p>
+      <p>Start typing in the form fields and your data may show up in the dropdowns to avoid such hell for databases as repetition.</p>
+      <h1 class="sec04">terms</h1>
       <p>Terms of use are such that:
         <ol>
-          <li>The relevant copyrights to any fact or/and true or false statement(s), or torfs hereinafter, saved in the website database are reserved to their original authors, whether translated into English or not;</i>
-            <li>Own copyrights are reserved in terms of coming up with the idea to accumulate such torfs with their sources, since at the time or writing this no website has, to the best of the website owner's knowledge, seemed to provide such quizzes that would feature linked materials to prove correctness and authenticity of the facts behind such torfs.</li>
-            <li>Further development of the website presupposes use of such torfs to the purposes that may seem fit.</li>
-            <li>Still any torfs added by users of the website are entirely at their discretion and to their liking, unless they breach any law or any relevant legislation, or offensive in nature or encourage discrimination or acts of terror; author(s) of the website are not responsible for all such torfs, but still shall do their utmost to check the database for such irregularities and violations.</li>
-            <li>Any torf under your email may be used by any third party, unless prohibited by you expressly in writing addressed to such a party.</li>
-            <li>No Wikipedia please.</li>
-            <li>By using this website you confirm you consent to these terms and to the use of your personal data, such as email.</li>
-              <li>More terms may follow with the course of time as the project develops or otherwise, without the site owner(s) notifying of these expressly in any form except via publication under this section.</li>
+          <li>The relevant copyrights to any fact or/and true or false statement(s), or torfs hereinafter (a term coined by KK), saved in the website database are reserved to their original authors, whether translated into English or not.</i>
+            <li>KK's copyrights are reserved in terms of coming up with the original idea to accumulate such torfs with their sources, since at the time or writing this no website has, to the best of KK's knowledge, seemed to provide such quizzes that would feature linked materials to prove correctness and authenticity of the facts behind such torfs.</li>
+            <li>KK may use other users' torfs saved in the database in builidng other content that may seem fit, with no hidnrance to their authors and with due reference. Should one wish to delete one's torf from the database after it has been used by KK or any third party, such a torf will be no longer possible to delete for ever unless the torf author decides to do the impossible on his or her or their own, which is entirely up to them.</li>
+            <li>Torfs added by users of the website are entirely at their discretion and to their liking, unless they breach any law or any relevant legislation, or are offensive in nature or encourage discrimination or acts of terror. KK is not responsible for all such torfs appearing in the database unnoticed, but still shall do KK's best to check the database for such irregularities and violations. Torf users are encouraged to give a helping hand in sieving such unwanted content both proactively and post factum.</li>
+            <li>Any torf under one's email may be used by any third party, unless prohibited by the torf author expressly in writing addressed to such a party, which is these two parties' business.</li>
+            <li>No Wikipedia(-related) information is to be used as valid and solid reference in the creation of one's torfs on this website.</li>
+            <li>Torfs as well as the comments to them will not exceed ... characters each. Reference style will be ... (link)</li>
+            <li>By using this website one confirms one's consent to these terms of use and to the use of one's personal data, such as email for identification.</li>
+              <li>More terms may follow with the course of time as the project develops or otherwise, without KK notifying the users of these expressly in any form except via publication under this section, in this numbered list.</li>
         </ol>
       </p>
-      <h1 class="sec06">@</h1>
-      <p>When not struck-through, this indicates that your email is happily familiar to the website and the PHP session is aware of it. With a strike-through, it means the opposite.</p>
+      <h1 class="sec05">@</h1>
+      <p>When not struck-through, this indicates that your email is happily familiar to the website and the current PHP session. With a strike-through, it means the opposite.</p>
       <footer>
         <script src="js/vendor/modernizr-3.6.0.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
